3744eaf3893dfba0170db23659925e8aa370197b
David Gonzalez <david.gonzalez.sotos@outlook.es>
2015-06-26 15:13:39 +0200

    Añadido listado de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,7 +14,7 @@ exports.answer = function(req,res){
 
 var models = require('../models/models.js');
 
-exports.question = function(req,res){
+/*exports.question = function(req,res){
 	models.Quiz.findAll().success(function(quiz){
 	  res.render('quizes/question',{pregunta:quiz[0].pregunta});	
 	})
@@ -30,4 +30,31 @@ exports.answer = function(req,res){
 		res.render('quizes/answer',{respuesta:'Incorrecto.'});
 	  }
 	})
+};*/
+//GET /quizes
+exports.index = function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+	  res.render('quizes/index',{quizes: quizes});	
+	})
+	
 };
+
+exports.show = function(req,res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+	  res.render('quizes/show',{quiz: quiz});	
+	})
+	
+};
+
+//GET /quizes/answer
+exports.answer = function(req,res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+	  if (req.query.respuesta ===quiz.respuesta){
+	  	res.render('quizes/answer',{quiz: quiz, respuesta:'Correcto.'});
+	  } else {
+		res.render('quizes/answer',{quiz: quiz, respuesta:'Incorrecto.'});
+	  }
+	})
+};
+
+
