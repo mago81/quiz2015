c5aa410d659b9d3c0d42d9cc3749edf1b604d45b
David Gonzalez <david.gonzalez.sotos@outlook.es>
2015-07-09 12:22:00 +0200

    Añadido Creador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -78,4 +78,22 @@ exports.answer = function(req,res){
 	})
 };
 
+//GET /quizes/new
+exports.new = function(req,res){
+	var quiz = models.Quiz.build(
+		{pregunta:"pregunta", respuesta: "respuesta"}
+	);
+	res.render('quizes/new',{quiz:quiz});
+};
+
+//POST /quizes/create
+exports.create = function(req,res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	console.log(quiz);
+
+	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})
+};
+
 
